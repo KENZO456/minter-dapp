1390c77b83999369a7f205f51d6abbcfa0352784
kenzo <kennyprince25.kp@gmail.com>
2022-04-16 19:58:05 -0700

    rinkeby testnet

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x8092fd917cd9FdCd179907B1526B26490EbA67dB"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
